089b15104d49e04a8c7bc447ee075a372aa7812c
Nabawi Arifin <nabawi.mgmt@gmail.com>
2023-03-16 19:33:26 +0700

    Added feature to add books and finished script to show every element in array

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const container = document.getElementById('library-container')
+const popup = document.getElementById('popup')
+const addBook = document.getElementById('add-book')
+
 let myLibrary = [
     {"author": "nabawi",
      "title": "AutoBiography of Bawi",
@@ -24,6 +28,35 @@ function Book(author, title, pages, read){
 const theHobbit = new Book('J.R.R. Tolkien', 'The Hobbit', '295', 'not red yet')
 console.log(theHobbit.info())
 
-function addBookToLibrary() {
-    let newBook = new Book()
+function openPopup() {
+    popup.classList.add("open-popup")
 }
+
+function addBookToLibrary() {
+    popup.classList.remove("open-popup")
+}
+
+function showLibrary(myLibrary) {
+    return myLibrary.map(myLibrary => `
+    <div class="card">
+            <div class="author">
+                <div>Author: ${myLibrary.author}</div>
+            </div>
+
+            <div class="title">
+                <div>Title: ${myLibrary.title}</div>
+            </div>
+
+            <div class="pages">
+                <div>Pages: ${myLibrary.pages}</div>
+            </div>
+
+            <div class="read">
+                <div>Read: </div>
+                <input type="checkbox">
+            </div>
+        </div>`).join('') + "</div>";
+
+}
+
+container.innerHTML = showLibrary(myLibrary)
