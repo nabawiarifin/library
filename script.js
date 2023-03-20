512ac0b78d4ef60348c8cccc8bc9d3d46b974aa7
Nabawi Arifin <nabawi.mgmt@gmail.com>
2023-03-20 21:32:47 +0700

    Fixed checkbox option to check if user has read book or not when adding a new book. Changed adding book to using constructors

--- script.js
+++ script.js
@@ -2,31 +2,26 @@ const container = document.getElementById('library-container')
 const popup = document.getElementById('popup')
 const addBook = document.getElementById('add-book')
 const submit = document.getElementById('submit')
+
 let myLibrary = [
     {"author": "nabawi",
      "title": "AutoBiography of Bawi",
      "pages": 25,
-     "read": true
+     "read": "checked"
     },
     {"author": "zahra",
      "title": "AutoBiography of zahra",
      "pages": 29,
-     "read": false
+     "read": ""
     }
 ]
 
-// function Book(author, title, pages, read){
-//     this.author = author
-//     this.title = title
-//     this.pages = pages
-//     this.read = read
-//     this.info = function() {
-//         console.log(`${author} by ${title}, ${pages} pages, read`)
-//     }
-// }
-
-// const theHobbit = new Book('J.R.R. Tolkien', 'The Hobbit', '295', 'not red yet')
-// console.log(theHobbit.info())
+function Book(author, title, pages, read){
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.read = checked(read)
+}
 
 container.innerHTML = showLibrary(myLibrary)
 
@@ -34,38 +29,53 @@ function openPopup() {
     popup.classList.add("open-popup")
 }
 
-function addBookToLibrary() {
-    let book = Array.from(document.querySelectorAll('.popup input')).reduce((acc,input) =>
-    ({...acc, [input.id]: input.value}), {})
-    myLibrary.push(book)
-    popup.classList.remove("open-popup")
-    container.innerHTML = showLibrary(myLibrary)
-}
-
 submit.addEventListener("click", function(event) {
     addBookToLibrary();
     event.preventDefault();
 })
+
+function addBookToLibrary() {
+    const author = document.getElementById('author').value
+    const title = document.getElementById('title').value
+    const pages = document.getElementById('pages').value
+    const read = document.getElementById('read')
+
+    const book = new Book(author, title, pages, read)
+    myLibrary.push(book)
+
+    popup.classList.remove("open-popup")
+    container.innerHTML = showLibrary(myLibrary)
+}
+
+function checked(checkbox){
+    if (checkbox.checked){
+        return "checked"
+    } else {
+        return ""
+    }
+}
+
 function showLibrary(myLibrary) {
     return myLibrary.map(myLibrary => `
     <div class="card">
-            <div class="author">
-                <div>Author: ${myLibrary.author}</div>
-            </div>
+        <button class="delete">X</button>
+        <div class="author">
+            <div>${myLibrary.author}</div>
+        </div>
 
-            <div class="title">
-                <div>Title: ${myLibrary.title}</div>
-            </div>
+        <div class="title">
+            <div>${myLibrary.title}</div>
+        </div>
 
-            <div class="pages">
-                <div>Pages: ${myLibrary.pages}</div>
-            </div>
+        <div class="pages">
+            <div>Pages: ${myLibrary.pages}</div>
+        </div>
 
-            <div class="read">
-                <div>Read: </div>
-                <input type="checkbox">
-            </div>
-        </div>`).join('') + "</div>";
+        <div class="read">
+            <div>Read: </div>
+            <input type="checkbox" ${myLibrary.read}>
+        </div>
+    </div>`).join('') + "</div>";
 
 }
 
